4ab0822494e2b9df9186b2f17677a6f6fd65280a
kalyan <128030009+kalyan3104@users.noreply.github.com>
2024-08-20 17:56:31 +0530

    tuples
    tuples

---
+++ tuples/tuples.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+	"fmt"
+)
+
+func powerSeries(a int) (int, int, int, int) {
+	return a*a, a*a*a, a*a*a*a, a*a*a*a*a
+}
+
+func main () {
+
+	var square int
+	var cube int
+	var quartic int
+    var quintic int
+
+	square,cube,quartic,quintic = powerSeries(5) 
+	fmt.Println("square:",square, "cube:",cube, "quartic:", quartic, "quintic:",quintic)
+}
